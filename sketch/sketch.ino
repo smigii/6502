5df06fcbfc9e6c70594ba5652738496c1cc23e79
GLaDOS <nem35b@protonmail.com>
2021-03-28 21:16:32 -0500

    Not sure what wasnt commited, whatever.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -21,25 +21,24 @@ void setup() {
 }
 
 void onClock() {
-  char output[15];
+  char output[32];
   
   unsigned int addr = 0;
   for(int i = 0; i < 16; i++){
     int bit = digitalRead(ADDR[i]) ? 1 : 0;
-    Serial.print(bit);
+//    Serial.print(bit);
     addr = (addr << 1) + bit;
   }
   
-  Serial.print("     ");
 
   unsigned int data = 0;
   for(int i = 0; i < 8; i++){
     int bit = digitalRead(DATA[i]) ? 1 : 0;
-    Serial.print(bit);
+//    Serial.print(bit);
     data = (data << 1) + bit;
   }
 
-  sprintf(output, " | %04x - %02x : %c", addr, data, digitalRead(RW) ? 'r' : 'w');
+  sprintf(output, "@ %04x -> %02x : %c", addr, data, digitalRead(RW) ? 'r' : 'w');
   
   Serial.println(output);
   
