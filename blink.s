75ff0d2884dd86226af9ffe0c02ad57b25c41861
GLaDOS <nem35b@protonmail.com>
2021-03-30 01:32:13 -0500

    Finished video 3

--- blink.s
+++ blink.s
@@ -1,2 +1,20 @@
+; NEED BLANK SPACE AT BEGINNING OF LINE
+
+	.org $8000		; Origin at addr 8000
+
+reset:
 	lda #$ff		; # means load the value $ff, no # would mean load value @ $00ff
 	sta $6002		; Store a at addr 6002
+
+	lda #$50
+	sta $6000
+
+loop:
+	ror				; Rotate bits right
+	sta $6000
+
+	jmp loop
+
+	.org $fffc
+	.word reset
+	.word $0000
---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# -Fbin			raw binary output
+# -dotdir		Needed to use .org and .word
+
+vasm6502_oldstyle -Fbin -dotdir blink.s
